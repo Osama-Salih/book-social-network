92208ffebecee17590fe1f117bc2334774f8ec18
Osama Salih <osamasalih.ca@gmail.com>
2025-10-13 16:44:00 +0300

    Create the Token entity and repository

---
+++ src/main/java/com/osama/book/user/Token.java
@@ -0,0 +1,26 @@
+package com.osama.book.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "token")
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiredAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
