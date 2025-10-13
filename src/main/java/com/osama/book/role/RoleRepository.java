0b668e4a3ed023b17c9d829ecd9d67bda1cc7467
Osama Salih <osamasalih.ca@gmail.com>
2025-10-13 16:29:11 +0300

    Implement user and role repositories

---
+++ src/main/java/com/osama/book/role/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.osama.book.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(final String name);
+}
---
+++ src/main/java/com/osama/book/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.osama.book.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(final String email);
+}
