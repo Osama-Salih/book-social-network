27bf10283401af4536e7aad28edc54d82d3bc7f7
Osama Salih <osamasalih.ca@gmail.com>
2025-10-17 17:01:02 +0300

    Create the feedback entity

---
+++ src/main/java/com/osama/book/feedback/FeedBack.java
@@ -0,0 +1,20 @@
+package com.osama.book.feedback;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+public class FeedBack {
+
+    private double not;
+    private String comment;
+}
