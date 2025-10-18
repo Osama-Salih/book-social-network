6ea921e375b64da69088fad2534703aa6110c453
Osama Salih <osamasalih.ca@gmail.com>
2025-10-18 08:13:45 +0300

    Add the application auditor aware

---
+++ src/main/java/com/osama/book/config/ApplicationAuditAware.java
@@ -0,0 +1,23 @@
+package com.osama.book.config;
+
+import com.osama.book.user.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class ApplicationAuditAware implements AuditorAware<Integer> {
+    @Override
+    public Optional<Integer> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null ||
+                !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken) {
+            return Optional.empty();
+        }
+
+        final User user = (User) authentication.getPrincipal();
+        return Optional.ofNullable(user.getId());
+    }
+}
